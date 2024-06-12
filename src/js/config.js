345bc218b706b7ee072f70732595828fdfabb258
Oskar-Wild <kondra996@gmail.com>
2024-06-12 19:08:04 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 15;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f16baf08-213c-4e6c-b3e2-ecfa0c606627';
+export const MODAL_CLOSE_SEC = 2;
